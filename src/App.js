b59172b64ced03e17968c4f2467f2cf711dc8f24
Gurjant Singh <c0700334@mylambton.ca>
2017-11-23 10:52:57 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import  './App.css';
+
+class App extends Component {
+
+  
+
+
+  render() {
+    return (
+        <div className="App">
+          <div className="container">
+            <div className="row">
+              <div className="col fbk">
+                  Our Form will be here
+              </div>
+              <div className="col lbk">
+                List
+              </div>
+              <div className="col-6 dbk">
+                  Details
+              </div>
+            </div>
+           
+          </div>
+      </div>
+    );
+  }
+}
+
+export default App;
